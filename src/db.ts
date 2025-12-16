0459104df1d88c9423458a03316e941f662cd543
pitsanukorn26 <pitsanukorn.mala@bumail.net>
2025-12-17 02:01:42 +0700

    Update

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
-const { MONGODB_URI } = process.env;
-if (!MONGODB_URI) throw new Error("MONGODB_URI is required");
+function getMongoUri(): string {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI is required");
+  return uri;
+}
+
+const mongoUri = getMongoUri();
 
 let conn: typeof mongoose | null = null;
 let connPromise: Promise<typeof mongoose> | null = null;
@@ -9,7 +14,7 @@ let connPromise: Promise<typeof mongoose> | null = null;
 export async function connectDb() {
   if (conn) return conn;
   if (!connPromise) {
-    connPromise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+    connPromise = mongoose.connect(mongoUri, { bufferCommands: false });
   }
   conn = await connPromise;
   return conn;
